e3f6ed5cabbbf3ead8678e0bd5c80f7261cc7687
Thomas Fletcher-Lobb <thomas.fletcher-lobb@students.plymouth.ac.uk>
2018-01-04 15:22:09 +0000

    server file changes

--- checkersServer.js
+++ checkersServer.js
@@ -1,15 +1,15 @@
-var http = require('http');
-var fs = require("fs");
+var http   = require('http');
+var url    = require('url');
+var fs     = require('fs');
 
 http.createServer(function(request, response)
 {
-  fs.readFile("login.html", function(err, data)
+  pathName  = url.parse(request.url).pathname;
+
+  fs.readFile('login.html', function(err, data)
   {
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200);
     response.write(data);
     response.end();
-  });
-}).listen(function()
-{
-  console.log('Sever listening')
+  })
 });
