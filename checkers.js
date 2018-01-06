bffaa670f44011debcf11a8303abbaf9ff7642af
Thomas Fletcher-Lobb <thomas.fletcher-lobb@students.plymouth.ac.uk>
2018-01-06 15:33:14 +0000

    Spent 4 hours achieving nothing

--- checkers.js
+++ checkers.js
@@ -1,10 +1,34 @@
-login()
-{
-  var usrnm = document.getElementsByID("username").value;
+//Login functions
+localStorage.usr = "guest001";
+var usrnm;
 
+function signUp()
+{
+  //usrnm = document.getElementByID("username");
+  window.location.href = "localhost/lobby.html";
 }
 
+function guestLogin()
+{
+  //usrnm = document.getElementByID("username");
+  //usrnm.innerHTML = "Guest";
+  window.location.href = "localhost/lobby.html";
+}
+
+function memberLogin()
+{
+  //usrnm = document.getElementByID("username");
+  window.location.href = "localhost/lobby.html";
+}
+
+function getUsr()
+{
+  document.getElementById('lobbyUser').innerHTML = usrnm.innerHTML;
+}
+
+
+//Game functions
 spawnPieces()
 {
-  board.row1.col1.
+  //board.row1.col1.
 }
