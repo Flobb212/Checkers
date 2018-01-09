ae7914315d74742d95f7b18325cd3e46ea27319a
Flobb212 <thomas.fletcher-lobb@students.plymouth.ac.uk>
2018-01-09 12:58:52 +0000

    Login and AJAX
    Login checks and page loading fixed. add AJAX call

--- checkers.js
+++ checkers.js
@@ -1,48 +1,95 @@
 //Login functions
-localStorage.usr = "guest001";
-var usrnm;
+localStorage.usr;
+var namesList = JSON.parse(localStorage["namesList"]);
+var passwordList = JSON.parse(localStorage["passwordList"]);
+
+if(namesList.length == 0)
+{
+  namesList[0] = "u0";
+  namesList[1] = "u1";
+  namesList[2] = "u2";
+
+  localStorage["namesList"] = JSON.stringify(namesList);
+}
+
+if(passwordList.length == 0)
+{
+  passwordList[0] = "p0";
+  passwordList[1] = "p1";
+  passwordList[2] = "p2";
+
+  localStorage["passwordList"] = JSON.stringify(passwordList);
+}
+
+function addUsername(thisName)
+{
+  console.log(namesList);
+  var temp = JSON.parse(localStorage["namesList"]);
+  temp.push(thisName);
+  localStorage["namesList"] = JSON.stringify(temp);
+  console.log(namesList);
+}
+
+
+
 
 function signUp(form)
 {
-  if(form.newUsername.value == "myuserid" && form.newPassword.value == "mypswrd")
+  if(form.newUsername.value == "" || form.newPassword.value == "")
   {
-    usrnm = form.newUsername.value;
-    window.open('lobby.html')
+    alert("Please enter a valid Password or Username");
   }
  else
  {
-   alert("Error Password or Username");
+   addUsername(form.newUsername.value)
+   localStorage.usr = form.newUsername.value;
+   window.location.href = "lobby.html";
  }
 }
 
 function guestLogin()
 {
-  //usrnm = document.getElementByID("username");
-  usrnm = "Guest";
+  localStorage.usr = "Guest001";
   window.location.href = "lobby.html";
 }
 
-function memberLogin()
+function memberLogin(form)
 {
-  if(form.newUsername.value == "myuserid" && form.newPassword.value == "mypswrd")
+  if(form.oldUsername.value == "myuserid" && form.oldPassword.value == "mypswrd")
   {
-    usrnm = form.newUsername.value;
-    window.open('lobby.html')
+    localStorage.usr = form.oldUsername.value;
+    window.location.href = "lobby.html";
   }
  else
  {
-   alert("Error Password or Username");
+   alert("Incorrect username or password entered");
  }
 }
 
 function getUsr()
 {
-  document.getElementById('lobbyUser').value = usrnm.innerHTML;
+
+  document.getElementById('lobbyUser').innerHTML = localStorage.usr;
 }
 
 
-//Game functions
-spawnPieces()
+//AJAX
+function doAJAX(url, cFunction)
 {
-  //board.row1.col1.
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    if (this.readyState == 4 && this.status == 200)
+    {
+      cFunction(this);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function myFunction(xhttp)
+{
+  document.getElementById("demo").innerHTML = xhttp.responseText;
 }
