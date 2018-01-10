2dbafb10eef71738b9ad3b0b81c32de75d79adfc
Thomas Fletcher-Lobb <thomas.fletcher-lobb@students.plymouth.ac.uk>
2018-01-10 23:51:26 +0000

    Worked on tests

---
+++ CheckersTest/SpecFlowFeatureLogin.feature
@@ -0,0 +1,14 @@
+﻿Feature: SpecFlowFeatureLogin	
+
+
+@mytag
+Scenario: Guest Login
+	Given I am on the login page
+	When I press procede as guest
+	Then the lobby page should load
+
+Scenario: Account Login
+	Given I am on the login page
+	And Enter an accounts details
+	When I press Login
+	Then the lobby page should load
---
+++ CheckersTest/StepDefinition.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace CheckersTest
+{
+    [Binding]
+    public sealed class StepDefinition
+    {
+        IWebDriver thisDriver = new ChromeDriver();       
+
+        [Given("I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            thisDriver.Url = "file:///C:/Users/Lobb/Desktop/Checkers/login.html";
+
+            if (thisDriver.FindElement(By.Id("newUsername")).Displayed == true)
+            {
+                Console.WriteLine("Correctly on login page");
+            }
+            else
+            {
+                Console.WriteLine("Not on Login page");
+            }
+       
+        }
+
+        [When("I press procede as guest")]
+        public void WhenIPressProcedeAsGuest()
+        {
+            var button = thisDriver.FindElement(By.Id("newUsername"));
+
+            button.Click();
+        }
+
+        [Then("the lobby page should load")]
+        public void ThenTheLobbyPageShouldLoad()
+        {
+            if (thisDriver.FindElement(By.Id("head")).Displayed == true)
+            {
+                Console.WriteLine("Correctly on lobby page");
+            }
+            else
+            {
+                Console.WriteLine("Not on Lobby page");
+            }
+        }
+    }
+}
